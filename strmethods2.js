e9a2abad433f2bfc324aa073346e5cbd20049788
himabindu1303 <neellapalahimabindu@gmail.com>
2024-10-29 22:27:58 +0530

    string methods part2

---
+++ strmethods2.js
@@ -0,0 +1,15 @@
+let str="Hello World!";
+let replacestr=str.replace("World!", "My dear friend")
+console.log(replacestr);
+
+let a="Hungry  ";
+console.log(a.repeat(10));
+
+let sentence="The quick brown fox jumps over the lazy dog. The fox is clever";
+console.log(sentence.lastIndexOf("fox"));
+
+let message="Hello world!"
+console.log(message.startsWith("hi"));
+
+let fileName="document.pdf"
+console.log(fileName.endsWith(".pdf"));
